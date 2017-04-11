70565dba00a26ca5e830bd511e54a169c3cc9a65
Rocio Avila <Rocio Avila>
2017-04-11 12:32:03 -0500

    cancha Basquet

---
+++ js/canchaBasquet.js
@@ -0,0 +1,6 @@
+var boton = document.getElementById("basquet");
+boton.addEventListener("click",mostrarCanchaBasquet);
+
+function mostrarCanchaBasquet(){
+  document.getElementById("basketball").style.display="block";
+}
